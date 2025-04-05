34471f48135c76b9f61d48144098d0e39da1a858
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-04-05 14:23:15 -0400

    Create FactorialUsingFor.java

---
+++ FactorialUsingFor.java
@@ -0,0 +1,19 @@
+// Write your name here
+
+// 1. Factorial using for loop
+
+import java.util.Scanner;
+
+public class FactorialUsingFor {
+    public static long calculateFactorial(int n) {
+        // Write your code here
+
+    }
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        System.out.println("Factorial of " + number + " is: " + calculateFactorial(number));
+    }
+}
